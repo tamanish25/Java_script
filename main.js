4c12f04da36378625b5f1c83c88798bb838a0cc7
manish25 <tamanish25@gmail.com>
2022-07-29 07:50:56 +0200

    second

--- main.js
+++ main.js
@@ -1,6 +1,21 @@
+"use strict";
 var name ='Manish';
 console.log(name);
 let address = "India";
 console.log(address);
 const num= 10;
 console.log(num + 25);
+
+//to remove spaces from string
+
+let name1 = "            Germany      ";
+console.log(name1.length);
+let name2= name1.trim();
+console.log(name2);
+console.log(name2.length);
+
+// to print the last letter of the string
+
+console.log(name2[name2.length-1]);
+
+
