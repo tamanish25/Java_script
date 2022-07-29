9fd6bb9caf696b4cc7a2a4b475961b7d1f0dc578
manish25 <tamanish25@gmail.com>
2022-07-29 10:30:46 +0200

    falsy values

--- second_hour1.js
+++ second_hour1.js
@@ -1,6 +1,3 @@
-// truthly and falsy values
-// falsy values
-// false ,' ',null,undefined ,0
 
 // if else condition
 let age = 19;
@@ -9,4 +6,20 @@ if (age >18){
 }
 else{
     console.log("User is not alowed")
+}
+
+//  remainder operator
+let num = 13;
+if(num%2== 0)
+{console.log("Number is even");}
+else {console.log("Number is odd");}
+
+// truthly and falsy values
+// falsy values
+// false ,' ',null,undefined ,0 is by default false
+
+let name ="";
+if (name){
+    console.log(name);}
+    else{console.log("Empty name");
 }
