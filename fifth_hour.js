7d9e6707494a27614d706142f243b0eccecf1c29
manish25 <tamanish25@gmail.com>
2022-08-02 18:35:39 +0200

    Object

---
+++ fifth_hour.js
@@ -0,0 +1,18 @@
+// Object (reference type_)
+const person={ name :"Harshit", age :22, hobbies:["Guitar","football","reading"]}
+console.log(person);
+console.log(person.name);
+console.log(person.age);
+ 
+// To add the key value pair in Object 
+
+ person.gender ='male';
+ console.log(person);
+
+ // Bracket Vs dot notation 
+
+ console.log(person["name"]);
+ console.log(person.name);
+
+
+
