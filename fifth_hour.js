04668521ea5428b801eb244516f174df8d739ab0
manish25 <tamanish25@gmail.com>
2022-08-02 19:03:47 +0200

    computed properties

--- fifth_hour.js
+++ fifth_hour.js
@@ -29,7 +29,7 @@ for (let key in person){
 }
 
 
-// Object funtion 
+// Object.keys funtion 
 
 console.log(Object.keys(person));  //*
 
@@ -38,9 +38,30 @@ console.log(Object.keys(person));  //*
 for(let key of Object.keys(person))
 {
     console.log(person[key]);
+
 }
 
 
+//Computed properties
+
+const key1 = 'objkey1';
+const key2 = 'objkey2';
+
+const value1 = "myvalue1";
+const value2 = "myvalue2";
+
+const obj = {
+[key1] : value1,
+[key2] : value2,
+
+}
+console.log(obj);
+
+const obj1={};
+obj1[key1] = value1;
+obj1[key2] = value2;
+
+console.log(obj1);
 
 
 
