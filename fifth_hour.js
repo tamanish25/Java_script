6e7cffd8ca2102117fbde408340290092d6304f6
manish25 <tamanish25@gmail.com>
2022-08-06 19:12:31 +0200

    arrow

--- fifth_hour.js
+++ fifth_hour.js
@@ -169,13 +169,59 @@ function searchthis(array1,numb1)
 {
     for(let num of array1)
     if (num===numb1){
-        count=0;
-    }
-    
+        return 0;
+          }
+    return 1;
 }
-searchthis([0,2,3,4,5,6,7,8,9,10,11,12,13,14],15);
-console.log(count);
+ans = searchthis([0,2,3,4,5,6,7,8,9,10,11,12,13,14],15);
+if (ans===0){
+    console.log("Number is present in the string");
+
+}
+
+else if(ans === 1){
+    console.log("Number is NOOOOOT present in the String");
+}
+
+// arrow functions
+// const singHappyBirthday = function(){
+//     console.log("happy birthday to you ......");
+// }
+
+const singHappyBirthday = () => {
+    console.log("happy birthday to you ......");
+}
+
+singHappyBirthday();
+
+const sumThreeNumbers = (number1, number2, number3) => {
+    return number1 + number2 + number3;
+}
+
+const ans = sumThreeNumbers(2,3,4);
+console.log(ans);
+
+// const isEven = function(number){
+//     return number % 2 === 0;
+// }
+
+const isEven = number => number % 2 === 0;
 
 
+console.log(isEven(4));
+
+const firstChar = anyString => anyString[0];
+
+console.log(firstChar("harshit"));
+
+
+const findTarget = (array, target) => {
+    for(let i = 0; i<array.length; i++){
+        if(array[i]===target){
+            return i;
+        }
+    }
+    return -1;
+}
 
 
