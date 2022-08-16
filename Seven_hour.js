974a60f2faef38a7042a9057f95fe07c31b3e705
manish25 <tamanish25@gmail.com>
2022-08-17 00:11:02 +0200

    some method

--- Seven_hour.js
+++ Seven_hour.js
@@ -205,3 +205,15 @@ numbers.forEach(multiplyBy2);  // forEach funtion passes index and item to the f
     console.log(ans);
    
 }
+{
+    // some method
+const userCart = [
+    {productId: 1, productName: "mobile", price: 12000},
+    {productId: 2, productName: "laptop", price: 22000},
+    {productId: 3, productName: "tv", price: 35000},
+    {productId: 3, productName: "macbook", price: 25000},
+]
+
+const ans = userCart.some((cartItem)=>cartItem.price > 100000);
+console.log(ans);
+}
