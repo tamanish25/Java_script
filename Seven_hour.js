eb14d049f97a601bd69a64c3b462cb8d02163794
manish25 <tamanish25@gmail.com>
2022-08-16 23:55:06 +0200

    Every Method

--- Seven_hour.js
+++ Seven_hour.js
@@ -191,3 +191,17 @@ numbers.forEach(multiplyBy2);  // forEach funtion passes index and item to the f
     const myUser = users.find((user)=>user.userId===3);
     console.log(myUser);
 }
+
+
+{// Every method
+    const userCart = [
+        {productId: 1, productName: "mobile", price: 12000},
+        {productId: 2, productName: "laptop", price: 22000},
+        {productId: 3, productName: "tv", price: 35000},
+    ]
+    
+    
+    const ans = userCart.every((cartItem)=>cartItem.price < 30000);
+    console.log(ans);
+   
+}
