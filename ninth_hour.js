11f2f2c37f7e7ec222f694003bbc64577bdaaf60
manish25 <tamanish25@gmail.com>
2022-08-20 21:11:04 +0200

    about undefined

---
+++ ninth_hour.js
@@ -0,0 +1,15 @@
+{
+    const user1 = {
+        firstName : "harshit",
+        age: 8,
+        about: function(){
+            console.log(this.firstName, this.age);
+        }   
+    }
+    
+    // don't do this mistake 
+    
+    user1.about();
+    const myFunc = user1.about;
+    myFunc();
+}
