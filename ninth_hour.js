d95f8604dc14c3a952f3960e93f5ff5723707298
manish25 <tamanish25@gmail.com>
2022-08-24 09:11:22 +0200

    correction

--- ninth_hour.js
+++ ninth_hour.js
@@ -10,6 +10,8 @@
     // don't do this mistake 
     
     user1.about();
-    const myFunc = user1.about;
+    //const myFunc = user1.about; // this is wrong
     myFunc();
+    const myFunc1 = user1.about.bind(user1); // this syntax is right
+    myFunc1();
 }
